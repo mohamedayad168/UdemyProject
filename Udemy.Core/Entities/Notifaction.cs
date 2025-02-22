14ddb138ad50736606198769c62e76441f655428
Mohamed Ayad <mo7amed3yad@gmail.com>
2025-02-22 19:24:36 +0200

    add notiaction class

---
+++ Udemy.Core/Entities/Notifaction.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Udemy.Core.Entities
+{
+    public class Notifaction
+    {
+        [Key]
+
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Content is Required")]
+
+        public string NotifactionContent { get; set; }
+    }
+}
