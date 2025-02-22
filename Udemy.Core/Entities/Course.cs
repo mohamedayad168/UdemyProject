f3d5001cd8f70919e67ed3429405a0947883e876
abdullrhman <3brhmaaan@gmail.com>
2025-02-22 19:19:32 +0200

    creating Answer , Course, CourseGoals, Instructor, Question Entities

---
+++ Udemy.Core/Entities/Course.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Udemy.Core.Entities;
+public class Course
+{
+    [StringLength(20)]
+    public string Title { get; set; }
+    [StringLength(50)]
+    public string Description { get; set; }
+    public string Status { get; set; }
+    public string Level { get; set; }
+    public decimal Discount { get; set; }
+    public decimal Price { get; set; }
+    public int Duration { get; set; }
+    [StringLength(20)]
+    public string Language { get; set; }
+    public DateTime CreationDate { get; set; }
+    [StringLength(50)]
+    public string ImageUrl { get; set; }
+    [StringLength(50)]
+    public string VideoUrl { get; set; }
+    public int No_Subscribers { get; set; }
+    public bool IsPaid { get; set; }
+    public bool IsApproved { get; set; }
+
+    public int CourseId { get; set; }
+    public int InstructorId { get; set; }
+}
