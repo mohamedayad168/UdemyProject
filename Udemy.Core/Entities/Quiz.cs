ba52373ac23de8e3a7ca1f2d9f6c66f56d3fe5b7
yousofo <jonabilmc@gmail.com>
2025-02-22 22:50:34 +0200

    Added Entites: StudentGrade - Quiz - QuizQuestion

---
+++ Udemy.Core/Entities/Quiz.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy.Core.Entities
+{
+    public class Quiz : BaseEntity
+    {
+        public Course Course { get; set; }
+        [Column(TypeName ="Course_Id")]
+        public int CourseId { get; set; }
+        [StringLength(50)]
+        public string Title { get; set; }
+    }
+}
---
+++ Udemy.Core/Entities/QuizQuestion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Udemy.Core.Enums;
+
+namespace Udemy.Core.Entities
+{
+    public class QuizQuestion
+    {
+        public Quiz Quiz { get; set; }
+        [Column(TypeName ="Quiz_Id")]
+        public int QuizId { get; set; }
+        public QuestionTypes QuestionType { get; set; }
+        [StringLength(50)]
+        [Column(TypeName = "Question_Text")]
+        public string QuestionText { get; set; }
+        [StringLength(50)]
+        [Column(TypeName = "Answer_Text")]
+        public string AnswerText { get; set; }
+    }
+}
