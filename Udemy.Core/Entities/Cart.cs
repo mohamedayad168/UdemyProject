9361960b577ce8d81553375495c2ba71fe6a9a5f
sara ebrahim sellow <sososellow1999@gmail.com>
2025-02-22 22:39:07 +0200

    four entity

---
+++ Udemy.Core/Entities/Cart.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy.Core.Entities
+{
+    public class Cart
+    {
+       
+    [Key]
+        public int CartId { get; set; }
+
+        [ForeignKey("Student")]
+        public int StudentId { get; set; }
+
+        [Required]
+        public DateTime DateCreated { get; set; }
+
+        public DateTime? LastModified { get; set; }
+
+        public decimal? Amount { get; set; }
+
+        // Navigation Property
+        public Student Student { get; set; }
+    } 
+}
+
--- Udemy.Core/Entities/Enrollment.cs
+++ Udemy.Core/Entities/Enrollment.cs
@@ -1,13 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Udemy.Core.Entities
 {
-    class Enrollment
-    {
 
+
+    public class Enrollment
+    {
+        [Key]
+
+        [ForeignKey("Student")]
+        public int StudentId { get; set; }
+
+        [Key]
+
+        [ForeignKey("Course")]
+        public int CourseId { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        public DateTime? CompletionDate { get; set; }
+
+        [Required]
+        public ProgressStatus Progress { get; set; }
+
+        // Navigation Properties
+        public Student Student { get; set; }
+        public Course Course { get; set; }
     }
+
+    public enum ProgressStatus
+    {
+        NotStarted,
+        InProgress,
+        Completed
+    }
+
 }
