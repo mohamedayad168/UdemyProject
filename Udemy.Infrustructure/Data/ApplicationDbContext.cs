a3b2a7ad7644bf23166249a249bb01ba5f33ab4a
abdullrhman <3brhmaaan@gmail.com>
2025-02-26 21:19:21 +0200

    add ApplicationDbContext

---
+++ Udemy.Infrustructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Udemy.Core.Entities;
+using Udemy.Infrustructure.Configuration;
+
+namespace Udemy.Infrustructure.Data;
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser , IdentityRole<int> , int>
+{
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(typeof(CourseGoalsConfiguration).Assembly);
+        base.OnModelCreating(builder);
+    }
+}
