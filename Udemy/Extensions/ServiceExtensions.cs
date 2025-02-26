08508da146f8a63c56b16c0099cbd3f2099eb8b0
Mohamed Ayad <mo7amed3yad@gmail.com>
2025-02-26 22:46:37 +0200

    edit service extension

--- Udemy/Extensions/ServiceExtensions.cs
+++ Udemy/Extensions/ServiceExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Udemy.Core.Entities;
-using Udemy.Infrustructure.Data;
+using Udemy.Infrustructure.ApplicationContext;
 
 namespace Udemy.Extensions;
 
@@ -9,7 +9,7 @@ public static class ServiceExtensions
 {
     public static void ConfigureIdentity(this IServiceCollection services)
     {
-        var builder = services.AddIdentity<ApplicationUser , IdentityRole<int>>(o =>
+        var builder = services.AddIdentity<ApplicationUser, IdentityRole<int>>(o =>
         {
             //o.Password.RequireDigit = true;
             //o.Password.RequireLowercase = false;
@@ -22,7 +22,7 @@ public static class ServiceExtensions
         .AddDefaultTokenProviders();
     }
 
-    public static void ConfigureSqlContext(this IServiceCollection services , IConfiguration configuration)
+    public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<ApplicationDbContext>(options =>
         {
