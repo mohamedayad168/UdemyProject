5fefec4ae97241ed20bcc032bf5ca8de4db19f7d
abdullrhman <3brhmaaan@gmail.com>
2025-03-07 01:46:48 +0200

    adding reading all the student and student by id in student controller

---
+++ Udemy.Presentation/Controllers/StudentsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Udemy.Core.ReadOptions;
+using Udemy.Service.IService;
+
+
+namespace Udemy.Presentations.Controllers;
+
+[ApiController]
+[Route("api/students")]
+public class StudentsController(IServiceManager serviceManager) : ControllerBase
+{
+    //?page=10&size=20
+    [HttpGet]
+    public async Task<IActionResult> GetAllStudents([FromQuery] RequestParamter? requestParamter)
+    {
+        var studentsDto = await serviceManager.StudentService.GetAllStudentAsync(false , requestParamter);
+        return Ok(studentsDto);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetStudentById(int id)
+    {
+        var studentDto = await serviceManager.StudentService.GetStudentByIdAsync(id , false);
+        return Ok(studentDto);
+    }
+}
