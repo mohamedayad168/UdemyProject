21f8f45eb963269c4e21dc8ca966e4b5e4256eb8
Mohamed Ayad <mo7amed3yad@gmail.com>
2025-03-08 19:34:25 +0200

    add mapper

--- Udemy.Service/AutoMapperConfigration/AutoMapperProfile.cs
+++ Udemy.Service/AutoMapperConfigration/AutoMapperProfile.cs
@@ -8,17 +8,8 @@ namespace Udemy.Service.AutoMapperConfigration
     {
         public AutoMapperProfile()
         {
-            CreateMap<StudentDto, Student>()
-                .ForMember(dest => dest.FirstName, opt => opt.Condition(src => !string.IsNullOrEmpty(src.FirstName)))
-                 .ForMember(dest => dest.LastName, opt => opt.Condition(src => !string.IsNullOrEmpty(src.LastName)))
-                 .ForMember(dest => dest.Email, opt => opt.Condition(src => !string.IsNullOrEmpty(src.Email)))
-                 .ForMember(dest => dest.Gender, opt => opt.Condition(src => !string.IsNullOrEmpty(src.Gender)))
-                 .ForMember(dest => dest.Bio, opt => opt.Condition(src => !string.IsNullOrEmpty(src.Bio)))
-                 .ForMember(dest => dest.Title, opt => opt.Condition(src => !string.IsNullOrEmpty(src.Title)))
-                 .ForMember(dest => dest.State, opt => opt.Condition(src => !string.IsNullOrEmpty(src.State)))
-                 .ForMember(dest => dest.City, opt => opt.Condition(src => !string.IsNullOrEmpty(src.City)))
-                 .ForMember(dest => dest.CountryName, opt => opt.Condition(src => !string.IsNullOrEmpty(src.CountryName)))
-                 .ForMember(dest => dest.Age, opt => opt.Condition(src => src.Age.HasValue));
+            CreateMap<StudentDto, Student>();
+
         }
     }
 }
