5f0b52e1944682a2fa78bd3209be80a13565b8a7
Mohamed Ayad <mo7amed3yad@gmail.com>
2025-03-08 21:06:32 +0200

    Add Social Media Repo

---
+++ Udemy.Core/IRepository/ISocialMediaRepository.cs
@@ -0,0 +1,11 @@
+﻿using Udemy.Core.Entities;
+
+namespace Udemy.Core.IRepository
+{
+    public interface ISocialMediaRepository
+    {
+        Task<SocialMedia> GetSocialMediaByIdAsync(int id, int userId);
+        Task<IEnumerable<SocialMedia>> GetSocialMediaByUserIdAsync(int userId);
+
+    }
+}
