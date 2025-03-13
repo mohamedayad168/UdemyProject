b59f44c2c2da7273b5c6286371d0d77abd0b20a8
sara ebrahim sellow <sososellow1999@gmail.com>
2025-03-13 03:02:19 +0200

    3

--- Udemy/Extensions/ServiceExtensions.cs
+++ Udemy/Extensions/ServiceExtensions.cs
@@ -36,7 +36,7 @@ public static class ServiceExtensions
     public static void ConfigureRepositoryManager(this IServiceCollection services)
     {
         services.AddScoped<IRepositoryManager, RepositoryManager>();
-        services.AddScoped<ICourseRequirementRepo, CourseRequirementRepo>();
+      
     }
     public static void ConfigureServiceManager(this IServiceCollection services)
     {
