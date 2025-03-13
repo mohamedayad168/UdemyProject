7a24108e3dda8e3aadbbeae18008fdda505343a9
Mohamed Ayad <mo7amed3yad@gmail.com>
2025-03-13 20:06:23 +0200

    a

---
+++ Udemy.Core/IRepository/IQuizRepository.cs
@@ -0,0 +1,11 @@
+﻿using Udemy.Core.Entities;
+using Udemy.Core.ReadOptions;
+
+namespace Udemy.Core.IRepository
+{
+    public interface IQuizRepository
+    {
+        Task<IEnumerable<Quiz>> GetQuizzesByCourseIdAsync(int courseId, RequestParamter requestParamter, bool trackChanges);
+        Task<IEnumerable<QuizQuestion>> GetQuestionsByQuizIdAsync(int quizId, RequestParamter requestParamter, bool trackChanges);
+    }
+}
---
+++ Udemy.Infrustructure/Repository/QuizRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Udemy.Core.Entities;
+using Udemy.Core.IRepository;
+using Udemy.Core.ReadOptions;
+
+namespace Udemy.Infrastructure.Repository
+{
+    public class QuizRepository : RepositoryBase<Quiz>, IQuizRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public QuizRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            this._context = dbContext;
+        }
+        public async Task<IEnumerable<Quiz>> GetQuizzesByCourseIdAsync(int courseId, RequestParamter requestParamter, bool trackChanges)
+        {
+            return await FindAll(trackChanges)
+                .Where(q => q.CourseId == courseId && q.IsDeleted == false)
+                .Skip((requestParamter.PageNumber - 1) * requestParamter.PageSize)
+                .Take(requestParamter.PageSize)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<QuizQuestion>> GetQuestionsByQuizIdAsync(int quizId, RequestParamter requestParamter, bool trackChanges)
+        {
+            return await _context.Set<QuizQuestion>()
+                .Where(q => q.QuizId == quizId)
+                .Skip((requestParamter.PageNumber - 1) * requestParamter.PageSize)
+                .Take(requestParamter.PageSize)
+                .ToListAsync();
+        }
+    }
+}
