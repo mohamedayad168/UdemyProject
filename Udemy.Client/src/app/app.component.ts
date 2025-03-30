5793f6ca8fbf9a09535d80853811bd68c186965f
sara ebrahim sellow <sososellow1999@gmail.com>
2025-03-30 16:28:48 +0200

    header

--- Udemy.Client/src/app/app.component.ts
+++ Udemy.Client/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatIcon, MatButton],
+  imports: [RouterOutlet, MatIcon, MatButton,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ Udemy.Client/src/app/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  isDarkMode = false;
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    document.body.classList.toggle('dark-mode', this.isDarkMode);
+  }
+}
