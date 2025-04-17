f2afa523069eae9199592df4998d87bfe19cf551
yousofo <jon772002@gmail.com>
2025-04-17 02:13:41 +0200

    - admin frontend: responsive header & sidenav --added pages: instructors, students, admins

---
+++ Client.Admin/src/app/pages/instructors-page/instructors-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-instructors-page',
+  imports: [],
+  templateUrl: './instructors-page.component.html',
+  styleUrl: './instructors-page.component.scss'
+})
+export class InstructorsPageComponent {
+
+}
---
+++ Client.Admin/src/app/pages/students-page/students-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-students-page',
+  imports: [],
+  templateUrl: './students-page.component.html',
+  styleUrl: './students-page.component.scss'
+})
+export class StudentsPageComponent {
+
+}
