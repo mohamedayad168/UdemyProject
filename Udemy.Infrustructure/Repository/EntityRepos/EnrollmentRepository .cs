b6080c47e41aba92c3861063d5764bba7fcd9bc4
Abdullrhman Elsayed <3brhmaaan@gmail.com>
2025-04-21 22:13:42 +0200

    Refactor enrollment methods for readability and efficiency
    Updated method signatures for improved readability by adding spaces in parameter lists. Replaced the original enrollment fetching logic with direct queries to `context.Enrollments`, including related `Course` and `Student` entities. This change enhances data retrieval practices in the repository.

--- Udemy.Infrustructure/Repository/EntityRepos/EnrollmentRepository .cs
+++ Udemy.Infrustructure/Repository/EntityRepos/EnrollmentRepository .cs
@@ -12,31 +12,39 @@ namespace Udemy.Infrastructure.Repository
     public class EnrollmentRepository(ApplicationDbContext context)
         : RepositoryBase<Enrollment>(context), IEnrollmentRepository
     {
-        public async Task<IEnumerable<Enrollment>> GetEnrollmentsByStudentIdAsync(int studentId, bool trackChanges)
+        public async Task<IEnumerable<Enrollment>> GetEnrollmentsByStudentIdAsync(int studentId , bool trackChanges)
         {
-            return await FindByCondition(e => e.StudentId == studentId, trackChanges)
-                .Include(e => e.Course)
+            //return await FindByCondition(e => e.StudentId == studentId, trackChanges)
+            //    .Include(e => e.Course)
+            //    .ToListAsync();
+            return await context.Enrollments.Where(e => e.StudentId == studentId)
+                .Include(x => x.Course)
                 .ToListAsync();
         }
 
-        public async Task<IEnumerable<Enrollment>> GetEnrollmentsByCourseIdAsync(int courseId, bool trackChanges)
+        public async Task<IEnumerable<Enrollment>> GetEnrollmentsByCourseIdAsync(int courseId , bool trackChanges)
         {
-            return await FindByCondition(e => e.CourseId == courseId, trackChanges)
-                .Include(e => e.Student)
-                .ToListAsync();
+
+            //return await FindByCondition(e => e.CourseId == courseId, trackChanges)
+            //    .Include(e => e.Student)
+            //    .ToListAsync();
+
+            return await context.Enrollments.Where(e => e.CourseId == courseId)
+                        .Include(x => x.Student)
+                        .ToListAsync();
         }
 
-        public async Task<Enrollment?> GetEnrollmentAsync(int studentId, int courseId, bool trackChanges)
+        public async Task<Enrollment?> GetEnrollmentAsync(int studentId , int courseId , bool trackChanges)
         {
-            return await FindByCondition(e => e.StudentId == studentId && e.CourseId == courseId, trackChanges)
+            return await FindByCondition(e => e.StudentId == studentId && e.CourseId == courseId , trackChanges)
                 .Include(e => e.Student)
                 .Include(e => e.Course)
                 .FirstOrDefaultAsync();
         }
 
-        public void EnrollCourseToStudent(int studentId, int courseId)
+        public void EnrollCourseToStudent(int studentId , int courseId)
         {
-            Create(new Enrollment() { StudentId = studentId, CourseId = courseId, Status = "" });
+            Create(new Enrollment() { StudentId = studentId , CourseId = courseId , Status = "" });
         }
     }
 }
